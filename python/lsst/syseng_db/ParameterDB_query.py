06307bd974ce9c2e6628e85969ca39271bec4a8f
danielsf <scott.f.daniel@gmail.com>
2016-03-18 14:30:16 -0700

    added docstring to ParameterDB_query.py

--- python/lsst/syseng_db/ParameterDB_query.py
+++ python/lsst/syseng_db/ParameterDB_query.py
@@ -6,11 +6,23 @@ from ParameterTree import Parameter
 __all__ = ["get_table_names", "get_column_names", "keyword_query"]
 
 class connection_cache(object):
+    """
+    This is essentially a thin wrapper around a dict of database connections.
+    When users want to connect to a SQLite database, they can use the
+    connect() method provided in this class and any existing connection
+    to that file will be used.  If non exists, a new connection will be
+    opened and stored for future use.
+    """
 
     def __init__(self):
         self._connection_dict = {}
 
     def connect(self, file_name):
+        """
+        Return a connection to the database specified by file_name.  If one
+        already exists, use that.  If not, open one, store it for future use,
+        and return it.
+        """
         if file_name in self._connection_dict:
             return self._connection_dict[file_name]
 
@@ -22,6 +34,9 @@ _global_connection_cache = connection_cache()
 
 
 def _convert_row_to_parameter(row):
+    """
+    Convert a raw of our SQLite database into a Parameter object.
+    """
     name = row[0]
     values = {'defaultValue':row[1],
                'upperValue':row[2],
@@ -45,6 +60,9 @@ def _convert_row_to_parameter(row):
     return Parameter(name, doc=doc, units=units, values=values, source=source)
 
 def get_table_names(db_name):
+    """
+    Return the names of the tables on the database specified by db_name.
+    """
     cursor = _global_connection_cache.connect(db_name).cursor()
     query = "SELECT name FROM sqlite_master WHERE type='table'"
     cursor.execute(query)
@@ -53,6 +71,10 @@ def get_table_names(db_name):
 
 
 def get_column_names(db_name, table_name):
+    """
+    Return the names of the rows in the table specified by table_name in
+    the database specified by db_name.
+    """
     if ')' in table_name:
         raise RuntimeError("%s is not a valid table_name" % table_name)
     cursor = _global_connection_cache.connect(db_name).cursor()
@@ -65,6 +87,11 @@ def get_column_names(db_name, table_name):
 
 
 def keyword_query(db_name, table_name, keyword_list):
+    """
+    Query the database db_name and table table_name for all Parameters
+    whose names or docstrings contain one of the keywords specified in
+    keyword_list.  Returns a list of Parameter objects.
+    """
     cmd = "SELECT * from %s" % table_name
     like_statement = None
     formatted_kw_list = []
