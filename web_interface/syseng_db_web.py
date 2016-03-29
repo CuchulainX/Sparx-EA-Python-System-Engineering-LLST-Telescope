cda5761d21cb9e64c9788996ac31e855f2efe8b5
danielsf <scott.f.daniel@gmail.com>
2016-03-29 14:38:32 -0700

    add function to return list of available versions over web interface

---
+++ web_interface/syseng_db_web.py
@@ -0,0 +1,17 @@
+import os
+from flask import Flask, render_template
+
+from lsst.syseng_db import syseng_db_config, get_column_names, get_table_names
+
+app = Flask(__name__)
+
+db_name = os.path.join(syseng_db_config["db_dir"], syseng_db_config["db_name"])
+
+@app.route("/versions")
+def afunction():
+    list_of_tables = get_table_names(db_name)
+    return render_template("list_template.html", name="Model Versions",
+                           input_list=list_of_tables)
+
+if __name__ == "__main__":
+    app.run()
