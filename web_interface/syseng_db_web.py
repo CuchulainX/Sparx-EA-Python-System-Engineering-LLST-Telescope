6677200f6c63d4dee2547627b9249f0476528f61
danielsf <scott.f.daniel@gmail.com>
2016-03-30 16:24:18 -0700

    added web functionality to display all available parameter names

--- web_interface/syseng_db_web.py
+++ web_interface/syseng_db_web.py
@@ -2,7 +2,7 @@ import os
 from flask import Flask, render_template, request
 
 from lsst.syseng_db import syseng_db_config, get_table_names
-from lsst.syseng_db import keyword_query
+from lsst.syseng_db import keyword_query, get_parameter_names
 
 app = Flask(__name__)
 
@@ -13,6 +13,20 @@ def display_versions():
     list_of_tables = get_table_names(db_name)
     return render_template("list_template.html", name="Model Versions",
                            input_list=list_of_tables)
+
+
+@app.route("/list-names", methods=['POST', 'GET'])
+def list_param_names():
+    name_list = []
+    model_version = None
+    if request.method == 'POST':
+        model_version = request.form['version']
+        name_list = get_parameter_names(db_name, model_version)
+
+    return render_template("parameter_name_search_form.html",
+                           model_version=model_version,
+                           input_list=name_list)
+
 
 @app.route("/search", methods=['POST', 'GET'])
 def search_params():
