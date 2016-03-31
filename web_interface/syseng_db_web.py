e1c02acae4ffd83f9d90cdcc1a5dcb9c1e8dd9eb
danielsf <scott.f.daniel@gmail.com>
2016-03-31 14:51:42 -0700

    added template to render error messages

--- web_interface/syseng_db_web.py
+++ web_interface/syseng_db_web.py
@@ -1,4 +1,5 @@
 import os
+import sqlite3
 from flask import Flask, render_template, request
 
 from lsst.syseng_db import syseng_db_config, get_table_names
@@ -21,7 +22,19 @@ def list_param_names():
     model_version = None
     if request.method == 'POST':
         model_version = request.form['version']
-        name_list = get_parameter_names(db_name, model_version)
+
+        if model_version == '':
+            message = "You must specify a model version to query." \
+                      +" Try clicking the 'List all available model verisons'" \
+                      +" button on the main page."
+            return render_template("error_template.html",
+                                   message=message)
+
+        try:
+            name_list = get_parameter_names(db_name, model_version)
+        except sqlite3.OperationalError, w:
+            return render_template("error_template.html",
+                                   message=w.message)
 
     return render_template("parameter_name_search_form.html",
                            model_version=model_version,
@@ -34,7 +47,19 @@ def search_params():
     if request.method == 'POST':
         kwrd = request.form['keyword'].replace(' ','').split(',')
         vv = request.form['version']
-        result_param_list = keyword_query(db_name, vv, kwrd)
+
+        if vv == '':
+            message = "You must specify a model version to query." \
+                      +" Try clicking the 'List all available model verisons'" \
+                      +" button on the main page."
+            return render_template("error_template.html",
+                                   message=message)
+
+        try:
+            result_param_list = keyword_query(db_name, vv, kwrd)
+        except sqlite3.OperationalError, w:
+            return render_template("error_template.html",
+                                   message=w.message)
 
     return render_template("keyword_search_form.html",
                            input_list=result_param_list)
