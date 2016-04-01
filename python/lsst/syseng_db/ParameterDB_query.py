1c43e21ded2dd9f5fe4d5bed3a4c44bc8c230d51
danielsf <scott.f.daniel@gmail.com>
2016-04-01 16:13:55 -0700

    add ability to specify what xml files you want to query from

--- python/lsst/syseng_db/ParameterDB_query.py
+++ python/lsst/syseng_db/ParameterDB_query.py
@@ -109,12 +109,15 @@ def get_parameter_names(db_name, table_name):
     return sorted([str(rr[0]) for rr in raw_results], key=lambda s: s.lower())
 
 
-def keyword_query(db_name, table_name, keyword_list):
+def keyword_query(db_name, table_name, keyword_list, xml_list=None):
     """
     Query the database db_name and table table_name for all Parameters
     whose names or docstrings contain one of the keywords specified in
     keyword_list.  Returns a list of Parameter objects.  Parameters are
     alphabetized by name (case-insensitive).
+
+    Option to limit search to data from .xml files specified in
+    xml_list.
     """
 
     if not os.path.exists(db_name):
@@ -128,9 +131,23 @@ def keyword_query(db_name, table_name, keyword_list):
         list_of_chars.append("%{}%".format(kw))
         list_of_chars.append("%{}%".format(kw))
         if like_statement is None:
-            like_statement = " WHERE name LIKE ? OR docstring like ?"
+            like_statement = " WHERE ( name LIKE ? OR docstring like ?"
         else:
             like_statement += " OR name LIKE ? OR docstring like ?"
+    like_statement += " )"
+
+    if xml_list is not None:
+        if len(xml_list)==1:
+            like_statement += " AND source = ?"
+            list_of_chars.append("%{}%".format(xml_list[0]))
+        else:
+            like_statement += " AND ( source = ?"
+            list_of_chars.append("{}".format(xml_list[0]))
+
+            for xml_file in xml_list[1:]:
+                like_statement += " OR source = ?"
+                list_of_chars.append("{}".format(xml_file))
+            like_statement += " )"
 
     cursor = _global_connection_cache.connect(db_name).cursor()
 
